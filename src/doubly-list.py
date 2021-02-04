b43f9e7d3716685167d7dfdbf0fbb5229d6c8909
codeYann <codeyan10@gmail.com>
2021-02-04 08:56:30 -0300

    Doubly Linked List implementation: append, display, pop, search, len

--- src/doubly-list.py
+++ src/doubly-list.py
@@ -1,31 +1,31 @@
 from node import Node
 
-
 class DoublyList:
     def __init__(self):
         self.head = None
         self.tail = None
         self.size = 0
 
-    def append(self, element, index):  # Complexity worst case: O(n)
+    def append(self, element, index):  # Complexidade: O(n)
+        # Insere um novo elemento na lista
         newNode = Node(element)
 
         if index <= self.size:
-            if self.head == None and self.tail == None:  # Complexity: O(1)
+            if self.head == None and self.tail == None:  # Complexidade: O(1)
                 self.head = newNode
                 self.tail = newNode
 
-            elif index == 0:  # Complexity: O(1)
+            elif index == 0:  # Complexidade: O(1)
                 newNode.next = self.head
                 self.head.prev = newNode
                 self.head = newNode
 
-            elif index == self.size:  # Complexity: O(1)
+            elif index == self.size:  # Complexidade: O(1)
                 self.tail.next = newNode
                 newNode.prev = self.tail
                 self.tail = newNode
 
-            else:  # Complexity: O(n)
+            else:  # Complexidade: O(n)
                 pointer = self.head
                 for _ in range(index):
                     pointer = pointer.next
@@ -39,16 +39,19 @@ class DoublyList:
             raise IndexError("List index out of range")
 
     def __len__(self):
+        # Retorna o tamanho da lista
         return self.size
 
-    def display(self):
+    def display(self): # Complexidade: O(n)
+        # Percorre a lista e mostra o valor de cada nó
         pointer = self.head
 
         while pointer:
             print(pointer.data)
             pointer = pointer.next
 
-    def search(self, element):
+    def search(self, element): # Complexidade: O(n)
+        # Busca por um elemento na lista e retorna sua posição
         pointer = self.head
         index = 0
 
@@ -62,7 +65,8 @@ class DoublyList:
         
         raise ValueError("{} is not in list".format(element))
     
-    def pop(self, index):
+    def pop(self, index): # Complexidade: O(n)
+        # Remove um nó da lista
         if index < self.size:
             if self.head.next == None:
                 print("Valor removido => {}".format(self.head.data))
@@ -71,17 +75,15 @@ class DoublyList:
 
             elif index == 0:
                 print("Valor removido => {}".format(self.head.data))
-                garbage = self.head
+                pointer = self.head
                 self.head = self.head.next
                 self.head.prev = None
-                del garbage
 
             elif index == self.size - 1:
-                garbage = self.tail
+                pointer = self.tail
                 self.tail = self.tail.prev
                 self.tail.next = None
-                print("Valor removido => {}".format(garbage.data))
-                del garbage
+                print("Valor removido => {}".format(pointer.data))
             
             else:
                 pointer = self.head
@@ -90,20 +92,8 @@ class DoublyList:
                 pointer.prev.next = pointer.next
                 pointer.next.prev = pointer.prev
                 print("Valor removido => {}".format(pointer.data))
-                del pointer
 
             self.size = self.size - 1
+            return pointer
         else:
             raise IndexError("List index out of range")
-Lista = DoublyList()
-
-Lista.append(15, 0)
-Lista.append(20, 1)
-Lista.append(14, 0)
-Lista.append(25, 1)
-
-Lista.display()
-
-Lista.pop(3)
-
-Lista.display()
