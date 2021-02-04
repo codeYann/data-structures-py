22eb3e24b0052d8a59e4bb30d8ee38264f6b0111
codeYann <codeyan10@gmail.com>
2021-02-04 08:56:51 -0300

    Linked List implementation: append, pop, display, index, len

--- src/linked-list.py
+++ src/linked-list.py
@@ -1,7 +1,5 @@
 # Linked List implementation
-
 from node import Node
-
 
 class LinkedList:
     def __init__(self):
@@ -9,23 +7,24 @@ class LinkedList:
         self.tail = None
         self.size = 0
 
-    def append(self, element, index):  # Complexity worst case: O(n)
+    def append(self, element, index):  # Complexidade worst case: O(n)
+        # Adiciona um nó na lista
         newNode = Node(element)
 
         if index <= self.size:
-            if self.head == None:  # Complexity: O(1)
+            if self.head == None:  # Complexidade: O(1)
                 self.head = newNode
                 self.tail = newNode
 
-            elif index == 0:  # Complexity: O(1)
+            elif index == 0:  # Complexidade: O(1)
                 newNode.next = self.head
                 self.head = newNode
 
-            elif index == self.size:  # Complexity: O(1)
+            elif index == self.size:  # Complexidade: O(1)
                 self.tail.next = newNode
                 self.tail = self.tail.next
 
-            else:  # Complexity: O(n)
+            else:  # Complexidade: O(n)
                 pointer = self.head
                 for _ in range(index - 1):
                     pointer = pointer.next
@@ -37,7 +36,8 @@ class LinkedList:
         else:
             raise IndexError("List index out of range")
 
-    def index(self, element):  # Complexity worst case: O(n)
+    def index(self, element):  # Complexidade: O(n)
+        # Retorna a posição de acordo com o valor passado!
         pointer = self.head
         i = 0
 
@@ -51,23 +51,24 @@ class LinkedList:
         raise ValueError("{} is not in list".format(element))
 
     def __len__(self):
+        # Retorna o tamanho da lista
         return self.size
 
-    def display(self):  # Complexity worst case: O(n)
+    def display(self):  # Complexidade: O(n)
+        # Percorre a lista e mostra todos os valores de cada nó
         pointer = self.head
 
         while pointer != None:
             print(pointer.data)
             pointer = pointer.next
 
-    def pop(self, index):
-
+    def pop(self, index): # Complexidade: O(n)
+        # Remove um nó da lista
         if index < self.size:
             if index == 0:
                 garbage = self.head
                 self.head = self.head.next
                 print("Valor removido {}".format(garbage.data))
-                del garbage
 
             elif index == self.size:
                 pointer = self.head
@@ -76,7 +77,6 @@ class LinkedList:
                 garbage = pointer.next
                 self.tail = pointer
                 print("Valor removido {}".format(garbage.data))
-                del garbage
 
             else:
                 pointer = self.head
@@ -85,20 +85,9 @@ class LinkedList:
                 garbage = pointer.next
                 pointer.next = pointer.next.next
                 print("Valor removido {}".format(garbage.data))
-                del garbage
 
             self.size = self.size - 1
+            return garbage
 
         else:
             raise IndexError("List index out of range")
-
-
-Lista = LinkedList()
-
-Lista.append(10, 0)
-Lista.append(15, 1)
-Lista.append(9, 0)
-
-Lista.display()
-Lista.pop(1)
-Lista.display()
